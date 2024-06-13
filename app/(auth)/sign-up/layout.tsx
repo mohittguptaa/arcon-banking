89ef26ccb27461014f8952017cd857729a6c35b3
mohittguptaa <mohitgupta8218@gmail.com>
2024-06-13 15:31:57 +0530

    first commit

---
+++ app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+     <main>
+      {children}
+     </main>
+    );
+  }
+  
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <main>
+    SIDEBAR
+    {children}
+   </main>
+  );
+}
